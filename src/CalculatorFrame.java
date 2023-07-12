2555fdae4624776e14bd28e166c6ecabd54107a5
Muhammadayub Erkinoff <muhammadayub2006@gmail.com>
2023-07-12 16:34:46 +0500

    CalculatorApp class is created CalculatorFrame class is created

--- src/CalculatorFrame.java
+++ src/CalculatorFrame.java
@@ -2,6 +2,7 @@ import java.awt.*;
 
 public class CalculatorFrame extends Frame {
     CalculatorFrame(){
+        setTitle("Scientific Calculator");
         setSize(400,400);
         setLayout(null);
         setVisible(true);
